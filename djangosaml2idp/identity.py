da3798ea6be76ce392f3d3f26aa75d0fdccbf7fe
Laetitia Pfaender <l.pfaender@learning-tribes.com>
2026-04-22 14:50:38 +0800

    support for SAML attributes set with UserProfile attributes

--- djangosaml2idp/identity.py
+++ djangosaml2idp/identity.py
@@ -3,4 +3,8 @@ def create_identity(user, sp_mapping):
     for user_attr, out_attr in sp_mapping.items():
         if hasattr(user, user_attr):
             identity[out_attr] = getattr(user, user_attr)
+        elif hasattr(user, "profile"):
+            profile = getattr(user, "profile")
+            if hasattr(profile, user_attr):
+                identity[out_attr] = getattr(profile, user_attr)
     return identity
